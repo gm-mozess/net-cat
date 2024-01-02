628a8b9789013b25cf8dc549c777eed5868f5e72
kallgueye <gueyekallamoussa@gmail.com>
2024-01-02 12:56:57 +0000

    display welcome

--- server/handler.go
+++ server/handler.go
@@ -26,11 +26,27 @@ func ServerTCP() {
 
 }
 
+
 func IncommingConnections(conn net.Conn) {
 	//time := time.Now().Format("01-01-1889 13:45:45 GHL")
 	conn.Write(WelcomeMessage())
+	conn.Write([]byte("[ENTER YOUR NAME]: "))
 
+	buffer := make([]byte, 1024)
+	for {
+		// Read data from the client
+		n, err := conn.Read(buffer)
+		if err != nil {
+		 fmt.Println("Error:", err)
+		 return
+		}
+	  
+		// Process and use the data (here, we'll just print it)
+		fmt.Printf("Received: %s", buffer[:n])
+	}
 }
+
+
 
 func WelcomeMessage() []byte {
 	file, err := os.Open("./pingoin.txt")
