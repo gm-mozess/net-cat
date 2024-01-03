d1050dff83f6a2aac675ac06e81b29697a856146
kallgueye <gueyekallamoussa@gmail.com>
2024-01-03 03:16:36 +0000

    into css

--- server/handler.go
+++ server/handler.go
@@ -1,13 +1,16 @@
 package server
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net"
 	"os"
+	"strings"
 )
 
 var Port string
+var maxConnect int
 
 func ServerTCP() {
 	listener, err := net.Listen("tcp", ":"+Port)
@@ -18,35 +21,46 @@ func ServerTCP() {
 	defer listener.Close()
 
 	for {
+		if maxConnect < 11{
+			
+			conn, err := listener.Accept()
+			CatchError(err)
+			maxConnect++
+			go IncommingConnections(conn)
+		}
 
-		conn, err := listener.Accept()
-		CatchError(err)
-		go IncommingConnections(conn)
+
 	}
 
 }
-
 
 func IncommingConnections(conn net.Conn) {
 	//time := time.Now().Format("01-01-1889 13:45:45 GHL")
 	conn.Write(WelcomeMessage())
 	conn.Write([]byte("[ENTER YOUR NAME]: "))
 
-	buffer := make([]byte, 1024)
 	for {
-		// Read data from the client
-		n, err := conn.Read(buffer)
-		if err != nil {
-		 fmt.Println("Error:", err)
-		 return
+		data := Reader(conn)
+		fmt.Println([]byte(data))
+		for data == "" {
+			conn.Write([]byte("[ENTER YOUR NAME]: "))
+			data = Reader(conn)
 		}
-	  
-		// Process and use the data (here, we'll just print it)
-		fmt.Printf("Received: %s", buffer[:n])
+
 	}
 }
 
+func Reader(conn net.Conn) string {
 
+	// Read data from the client
+	netData, err := bufio.NewReader(conn).ReadString('\n')
+	netData = strings.Trim(netData, "\n")
+
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+	return netData
+}
 
 func WelcomeMessage() []byte {
 	file, err := os.Open("./pingoin.txt")
